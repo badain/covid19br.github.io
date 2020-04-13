a81258f0509304468a5e52ddcf767e61965cff54
badain <rafabadain@gmail.com>
2020-04-13 19:51:31 -0300

    estados automation

---
+++ js/changeState.js
@@ -0,0 +1,13 @@
+// Declaration
+// "Nome do Estado" <-> "UF"
+var estados = 
+[
+    {
+        uf:   "SP",
+        verbose: "São Paulo"
+    },
+    {
+        uf:   "RJ",
+        verbose: "Rio de Janeiro"
+    }
+]; 
